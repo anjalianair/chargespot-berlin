f3bd1b8b0cf5cb0982d36929ca61f8c5dda87a70
Anjali Ambakkat Nair <anjalianair146@gmail.com>
2026-09-10 18:14:38 +0530

    Allow frontend access from local development addresses

--- api/app/main.py
+++ api/app/main.py
@@ -17,11 +17,11 @@ app = FastAPI(
 app.include_router(auth_router)
 app.include_router(proposals_router)
 app.include_router(analysis_router)
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
